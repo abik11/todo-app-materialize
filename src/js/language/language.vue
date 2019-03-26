76fe788cc0ad58752cd5b6e6082548c0bd59dae7
abik11 <abik11@gmail.com>
2019-03-26 22:30:38 +0100

    updating webpack from 2.5 to 4

--- src/js/language/language.vue
+++ src/js/language/language.vue
@@ -17,7 +17,7 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .lang-box {
   margin: 10px;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 var path = require('path');
 var webpack = require('webpack');
 var glob = require('glob-all');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var VueLoaderPlugin = require('vue-loader/lib/plugin');
+var MiniCssExtractPlugin = require("mini-css-extract-plugin");
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var CleanWebpackPlugin = require('clean-webpack-plugin');
 var WebpackNotifierPlugin = require('webpack-notifier');
@@ -11,12 +12,9 @@ var isProduction = process.env.NODE_ENV === 'production';
 
 //Dev and prod conf for SASS and CSS
 var cssDev = ['style-loader', 'css-loader', 'resolve-url-loader', 'sass-loader'];
-var cssProd = ExtractTextPlugin.extract({
-  fallback: 'style-loader', 
-  loader: ['css-loader', 'resolve-url-loader', 'sass-loader']
-  //SASS goes through sass-loader, then resolve-url-loader, then css-loader
-  //The result is extracted to a file (see plugin configuration)
-});
+var cssProd = [MiniCssExtractPlugin.loader, 'style-loader', 'css-loader', 'resolve-url-loader', 'sass-loader']
+//SASS goes through sass-loader, then resolve-url-loader, then css-loader
+//The result is extracted to a file (see plugin configuration)
 var cssConfig = isProduction ? cssProd : cssDev;
 
 module.exports = {
@@ -38,47 +36,54 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          presets: ['es2015']
-        }
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015']
+          }
+        },
       },
       {
         test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          loaders: { 
-            //Using babel-loader for javascript in .vue components
-            js: 'babel-loader?presets[]=es2015',
-            //Enabling SASS transpilation in .vue components
-            scss: 'vue-style-loader!css-loader!sass-loader',
-            sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+        use: {
+          loader: 'vue-loader',
+          options: {
+            loaders: {
+              //Using babel-loader for javascript in .vue components
+              js: 'babel-loader?presets[]=es2015',
+              //Enabling SASS transpilation in .vue components
+              scss: 'vue-style-loader!css-loader!sass-loader',
+              sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+            }
           }
         }
       },
       {
         test: /\.scss$/,
-        use: cssConfig 
+        use: cssConfig
       },
       {
         test: /\.html$/,
         use: ['html-loader']
-      },      
+      },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]',
-          outputPath: 'img/'
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'img/'
+          }
         }
       },
-      { 
-        test: /\.(woff2?|ttf|eot|svg)$/, 
-        loader: 'url-loader?limit=10000&name=fonts/[name].[ext]' 
+      {
+        test: /\.(woff2?|ttf|eot|svg)$/,
+        use: { loader: 'url-loader?limit=10000&name=fonts/[name].[ext]' }
       },
     ]
   },
-  plugins: [   
+  plugins: [
+    new VueLoaderPlugin(),
     new webpack.ProvidePlugin({
       jQuery: 'jquery',
       $: 'jquery',
@@ -86,12 +91,9 @@ module.exports = {
       lodash: 'lodash',
       _: 'lodash',
       axios: 'axios'
-    }),    
-    new ExtractTextPlugin({
-      filename: '/css/[name].css',
-      disable: !isProduction 
-      //Turning off Extract Text Plugin for dev mode
-      //To enable Hot Module Replacement
+    }),
+    new MiniCssExtractPlugin({
+      filename: "/css/[name].css",
     }),
     new PurifyCSSPlugin({
       paths: glob.sync([
@@ -106,12 +108,12 @@ module.exports = {
         collapseWhitespace: isProduction
       }
     }),
-    new webpack.optimize.UglifyJsPlugin({ minimize: isProduction }),
-    new CleanWebpackPlugin(['.tmp', 'dist']),
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ['.tmp', 'dist']
+    }),
     new WebpackNotifierPlugin(),
-    new webpack.HotModuleReplacementPlugin(), //Hot Module Replacement
-    new webpack.NamedModulesPlugin()          //Hot Module Replacement
-  ],  
+    new webpack.HotModuleReplacementPlugin() //Hot Module Replacement
+  ],
   resolve: {
     alias: {
       'vue': 'vue/dist/vue.js'
